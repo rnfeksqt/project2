597bb9173f25fcf38c89737b81a6678759054b5a
rnfeksqt <eaglet1115@gmail.com>
2025-09-30 21:25:45 +0900

    3 day
    회전회오리

--- Assets/Script/Ship.cs
+++ Assets/Script/Ship.cs
@@ -7,17 +7,41 @@ public class Ship : MonoBehaviour
     [SerializeField] float speed = 10f;
     [SerializeField] float xRange = 5f;
     [SerializeField] float yRange = 5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float positionPitchFactor = 0f;
+    [SerializeField] float controlPitchFactor = 0f;
+    [SerializeField] float postionYawFactor = 0f;
+    [SerializeField] float controlYawFactor = 0f;
+    [SerializeField] float controlRollFactor = 0f;
 
+    float hor;
+    float ver;
+    
     // Update is called once per frame
     void Update()
     {
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
+        PrecessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessRotation()
+    {
+        float pitchPos = transform.localPosition.y * positionPitchFactor;
+        float pitchCon = ver * controlPitchFactor;
+        float pitch = pitchPos + pitchCon;
+
+        float yawPos =  transform.localPosition.x * postionYawFactor;
+        float yawCon =  hor * controlYawFactor;
+
+        float yaw =  yawPos + yawCon;
+
+        float roll = hor * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void PrecessTranslation()
+    {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
 
         float xOffset = hor * Time.deltaTime * speed;
         float yOffset = ver * Time.deltaTime * speed;
@@ -28,6 +52,6 @@ public class Ship : MonoBehaviour
         float clampedXpos = Mathf.Clamp(newXpos, -xRange, xRange);
         float clampedYpos = Mathf.Clamp(newYpos, -yRange, yRange);
 
-        transform.localPosition = new Vector3(clampedXpos, clampedYpos , transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
     }
 }
